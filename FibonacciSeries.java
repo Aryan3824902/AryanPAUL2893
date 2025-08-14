0afc92c23eed57d9e59e51dd85686d4b21a579b0
Aryan3824902 <iklbipop@gmail.com>
2025-08-14 11:14:01 +0530

    FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -6,10 +6,7 @@ public class FibonacciSeries {
 
         System.out.print("Enter number of terms: ");
         int n = sc.nextInt();
-
         int a = 0, b = 1;
-
-        System.out.print("Fibonacci Series: ");
         for (int i = 0; i < n; i++) {
             System.out.print(a + " ");
             int next = a + b;
